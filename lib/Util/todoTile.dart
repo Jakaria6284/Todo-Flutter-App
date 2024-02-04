1003d1d75f06b41e54972a262ba2740a2044e2ab
jakaria6284 <kamal.jakaria.6284mia@gmail.com>
2024-02-04 23:28:11 +0600

    second commit

--- lib/Util/todoTile.dart
+++ lib/Util/todoTile.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-class todoTile extends StatelessWidget {
+
+class TodoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
+  final ValueChanged<bool?> ?onChanged;
 
-  Function(bool?)?onChanged;
-   todoTile({super.key,
-     required this.taskName,
-     required this.taskCompleted,
-     required this.onChanged,
-
-
-   });
-
-
+  TodoTile({
+    Key? key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,16 +18,25 @@ class todoTile extends StatelessWidget {
       padding: const EdgeInsets.all(25.0),
       child: Container(
         padding: EdgeInsets.all(25),
-        child:Row(children: [
-          Text(taskName,style: TextStyle(fontWeight:FontWeight.bold,fontSize: 18)),
-          Checkbox(value: taskCompleted, onChanged: onChanged)
-
-        
-        
-        ]),
+        child: Row(
+          children: [
+            Text(
+              taskName,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                decoration: taskCompleted ? TextDecoration.lineThrough : null,
+              ),
+            ),
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+            )
+          ],
+        ),
         decoration: BoxDecoration(
           color: Colors.amber,
-          borderRadius: BorderRadius.circular(12)
+          borderRadius: BorderRadius.circular(12),
         ),
       ),
     );
--- lib/WeatherPage.dart
+++ lib/WeatherPage.dart
@@ -1,38 +1,103 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:weather/Util/todoTile.dart';
+
 class WeatherPage extends StatefulWidget {
-  const WeatherPage({super.key});
+  const WeatherPage({Key? key}) : super(key: key);
 
   @override
   State<WeatherPage> createState() => _WeatherPageState();
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  List<List<dynamic>> toDoList = [
+    ["make tutorial", false],
+    ["Do Exercise", false]
+  ];
 
+  void checkBoxChange(bool? value, int index) {
+    setState(() {
+      if (value != null) {
+        toDoList[index][1] = !toDoList[index][1];
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-       appBar: AppBar(
-         backgroundColor: Colors.amber,
-         title: Center(child: Text('To-Do List',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)),
-       ),
-      body: ListView(
-        children: [
-          todoTile(
-            taskName: "make tutorial",
-            taskCompleted: true,
-            onChanged: (p0){},
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Center(
+          child: Text(
+            'To-Do List',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
           ),
-          todoTile(
-            taskName: "make tutorial",
-            taskCompleted: false,
-            onChanged: (p0){},
-          ),
-        ],
-      )
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.place_outlined),
+        onPressed: ()
+        {
+          createNewTask(context);
+        },
+      ),
+
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChange(value, index),
+          );
+        },
+      ),
 
     );
   }
+
+
+  void createNewTask(BuildContext context) {
+    TextEditingController taskController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add New Task'),
+          content: TextField(
+            controller: taskController,
+            decoration: InputDecoration(hintText: 'Enter task name'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                String newTask = taskController.text.trim();
+                if (newTask.isNotEmpty) {
+                  // Add the new task to your task list
+                  setState(() {
+                    toDoList.add([newTask, false]);
+                  });
+                }
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
 }
